21974cba4d616126a0e861c323edc5cab6a930a1
stega <stegallj4@gmail.com>
2024-02-29 02:19:43 -0500

    Grammar fixes

--- src/components/Footer/Newsletter.jsx
+++ src/components/Footer/Newsletter.jsx
@@ -8,7 +8,7 @@ const Newsletter = () => (
     <div className="app__newsletter-heading">
       <SubHeading title="Newsletter" />
       <h1 className="headtext__cormorant">Subscribe to Our Newsletter!</h1>
-      <p className="p__opensans">And never miss latest Updates!</p>
+      <p className="p__opensans">And never miss the latest Updates!</p>
     </div>
     <div className="app__newsletter-input flex__center">
       <input type="email" placeholder='Enter your email address' />
--- src/container/Menu/SpecialMenu.jsx
+++ src/container/Menu/SpecialMenu.jsx
@@ -7,8 +7,8 @@ import './SpecialMenu.css';
 const SpecialMenu = () => (
   <div className="app__specialMenu flex__center section__padding" id="menu">
     <div className="app__specialMenu-title">
-      <SubHeading title="Menu that fits your palatte" />
-      <h1 className="headtext__cormorant">Today&apos;s Special</h1>
+      <SubHeading title="Menu that fits your palate" />
+      <h1 className="headtext__cormorant">Today's Special</h1>
     </div>
 
     <div className="app__specialMenu-menu">
